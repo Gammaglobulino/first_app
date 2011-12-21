45fe89875b75bcc462de1c782d47d8e6bc484137
Gammaglobulino <gammaglobulino@ubuntu.ubuntu-domain>
2011-12-21 01:02:57 +0100

    primo committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8fa147c5869f6155eb49b1fab8120a9312466771829085dcfdbb48d13ab0f7af8f2880bb454f969075784ed550a70f4f7a00d0b8c3d1fe49e6269e6526889f8e'
